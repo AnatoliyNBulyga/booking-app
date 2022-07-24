97f36b20849848d88b682c09cacf4d9f3c1f00c2
AnatoliyNBulyga <anatoliynbulyga@gmail.com>
2022-07-24 21:12:29 +0300

    Added client side

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import hotelsRoute from './routes/hotels.js';
 import roomsRoute from './routes/rooms.js';
 import usersRoute from './routes/users.js';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 const app = express()
 const PORT = process.env.PORT || 8800
@@ -25,6 +26,7 @@ mongoose.connection.on('disconnected', () => console.log('mongoDB disconnected!'
 mongoose.connection.on('connected', () => console.log('mongoDB connected!'))
 
 // middlewares
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,13 @@
 import express from "express"
-import {createHotel, deleteHotel, getHotel, getHotels, updateHotel} from "../controllers/hotel.js";
+import {
+    countByCity,
+    countByType,
+    createHotel,
+    deleteHotel,
+    getHotel,
+    getHotels,
+    updateHotel
+} from "../controllers/hotel.js";
 import {verifyAdmin} from "../utils/verify.js";
 
 const router = express.Router()
@@ -11,8 +19,12 @@ router.put('/:id', verifyAdmin, updateHotel)
 // DELETE
 router.delete('/:id', verifyAdmin, deleteHotel)
 // GET
-router.get('/:id', getHotel)
+router.get('/find/:id', getHotel)
 // GET ALL
 router.get('/', getHotels)
+// Others
+router.get('/count-by-city', countByCity)
+// router.get('/count-by-city', countByCity)
+router.get('/count-by-type', countByType)
 
 export default router
